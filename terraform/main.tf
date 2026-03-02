b4317329f25fd1fc72ce277d114cb703b2a09413
Freddy Cruz <freddycr42@Freddys-MacBook-Pro.local>
2026-03-01 20:49:00 -0400

    chore(terraform): Remove unused nat gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,29 +11,6 @@ resource "azurerm_resource_group" "main" {
   tags     = local.tags
 }
 
-module "nat_gateway" {
-  source  = "Azure/avm-res-network-natgateway/azurerm"
-  version = "~> 0.2.1"
-
-  name                = module.naming.nat_gateway.name
-  location            = var.azure_location
-  resource_group_name = azurerm_resource_group.main.name
-
-  sku_name = "Standard"
-
-  public_ips = {
-    primary = {
-      name = module.naming.public_ip.name
-    }
-  }
-
-  public_ip_configuration = {
-    sku = "Standard"
-  }
-
-  tags = local.tags
-}
-
 module "vnet" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
   version = "~> 0.17.0"
@@ -51,9 +28,6 @@ module "vnet" {
       name                            = module.naming.subnet.name
       address_prefix                  = local.vnet_subnets[local.environment][0]
       default_outbound_access_enabled = false
-      nat_gateway = {
-        id = module.nat_gateway.resource_id
-      }
     }
   }
 
